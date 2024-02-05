f6eb23eca65f53fc78217d8aeebb525a8a836bfe
Pedro Soprano <vonhishi@gmail.com>
2024-02-05 12:16:32 -0400

    integrando o frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,146 @@
-import { Autocomplete, Box, Button, TextField, Typography } from "@mui/material";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Autocomplete, Box, Button, Checkbox, FormControlLabel, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [causes, setCauses] = useState([])
+  const [dominios, setDominios] = useState([])
+  const [classes, setClasses] = useState([])
+  const [diagnosticos, setDiagnosticos] = useState([])
+
+  const [formData, setFormData] = useState<any>({
+    diagnostico: "",
+    resultadosEsperados: "",
+    codEnf: "",
+    causas: [],
+    dominio: "", 
+    classe: "",
+    intervencoes: [],
+  });
+
+  const isObjectEmpty = (obj: any) => {
+    for (const key in obj) {
+      if (obj[key] === '' || (Array.isArray(obj[key]) && obj[key].length === 0)) {
+        return true; 
+      }
+    }
+    return false; 
+  };
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleCausasChange = (event: any, values: any) => {
+    setFormData({
+      ...formData,
+      causas: values.map((value: any) => ({ id: value.id })),
+    });
+  };
+
+  const handleDiagnosticoChange = (event: any, value: any) => {
+    setFormData({
+      ...formData,
+      diagnostico:  value.id,
+    });
+  }
+
+  // const handleIntervencoesChange = (event) => {
+  //   const descricao = event.target.value;
+  //   setFormData({
+  //     ...formData,
+  //     intervencoes: [{ descricao, opcional: false }],
+  //   });
+  // };
+
+  const handleSave = () => {
+    if(isObjectEmpty(formData)){
+      toast.error("Todos os campos devem ser preenchidos")
+    }else{
+      console.log(formData)
+      api.post("/api/diagnostico").then(() => {
+        toast.success("Diagnóstico cadastrado com sucesso!")
+      }).catch(() => {
+        toast.error("Ocorreu um erro ao cadastrar o diagnóstico")
+      })
+    }
+  };
+
+
+  const api = axios.create({
+    baseURL: import.meta.env.VITE_PORT_PROJECT_BACKEND,
+  });
+
+  const getCauses = () => {
+    api.get("/api/cause").then((res) => {
+      setCauses(res.data)
+    })
+  }
+
+  const getDominios = () => {
+    api.get("/api/dominios").then((res) => {
+      setDominios(res.data)
+    })
+  }
+
+  const getClasses = () => {
+    api.get("/api/classes").then((res) => {
+      setClasses(res.data)
+    })
+  }
+
+  const getDiagnosticos = () => {
+    api.get("/api/diagnostico").then((res) => {
+      setDiagnosticos(res.data)
+    })
+  }
+
+  useEffect(() => {
+    getCauses()
+    getDominios()
+    getClasses()
+    getDiagnosticos()
+  }, [])
+
+  const [intervencoes, setIntervencoes] = useState<any[]>([]);
+  const [intervencaoAtual, setIntervencaoAtual] = useState({
+    descricao: '',
+    opcional: false,
+  });
+
+  const handleDescricaoChange = (event: any) => {
+    setIntervencaoAtual({
+      ...intervencaoAtual,
+      descricao: event.target.value,
+    });
+  };
+
+  const handleOpcionalChange = (event: any) => {
+    setIntervencaoAtual({
+      ...intervencaoAtual,
+      opcional: event.target.checked,
+    });
+  };
+
+  const adicionarIntervencao = () => {
+    setIntervencoes([...intervencoes, intervencaoAtual]);
+    setFormData({
+      ...formData,
+      intervencoes: [...intervencoes, intervencaoAtual]
+    })
+    setIntervencaoAtual({
+      descricao: '',
+      opcional: false,
+    });
+  };
+
   return (
     <Box
       sx={{
@@ -25,29 +165,41 @@ function App() {
           overflowY: "auto"
         }}
       >
-        <Typography sx={{marginBottom: "20px", fontSize: "25px"}}>
+        <Typography sx={{marginBottom: "20px", fontSize: "25px", color: "#01828e", fontWeight: 900}}>
           Cadastro de causas e diagnósticos
         </Typography>
 
 
-        <TextField
-          name="resultadosEsperados"
-          placeholder="Resultados esperados"
+        <Autocomplete
           sx={{ marginBottom: 2 }}
+          id="diagnostico"
+          options={diagnosticos}
+          onChange={handleDiagnosticoChange}
+
+          getOptionLabel={(option: any) => option.nome}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Diagnóstico"
+              placeholder="Selecione"
+            />
+          )}
         />
 
       <TextField
-          name="codEnd"
+          name="codEnf"
           placeholder="Código enfermagem"
           sx={{ marginBottom: 2 }}
+          onChange={handleInputChange}
         />
 
         <Autocomplete
           multiple
           sx={{ marginBottom: 2 }}
           id="causas"
-          options={top100Films}
-          getOptionLabel={(option) => option.title}
+          options={causes}
+          onChange={handleCausasChange}
+          getOptionLabel={(option: any) => option.descricao}
           renderInput={(params) => (
             <TextField
               {...params}
@@ -57,167 +209,79 @@ function App() {
           )}
         />
 
-        <TextField
-          name="dominio"
-          placeholder="Domínio"
-          sx={{ marginBottom: 2 }}
-        />
-
-        <TextField
-          name="classe"
-          placeholder="Classe"
-          sx={{ marginBottom: 2 }}
-
-        />
 
         <Autocomplete
-          multiple
-          id="intervencoes"
+          id="dominio"
+          options={dominios}
           sx={{ marginBottom: 2 }}
-          options={top100Films}
-          getOptionLabel={(option) => option.title}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Intervenções"
-              placeholder="Selecione"
-            />
-          )}
+          getOptionLabel={(option: any) => option.nome}
+          onChange={(event, value: any) => setFormData({ ...formData, dominio: (value.id) })}
+          renderInput={(params) => <TextField {...params} label="Domínio" />}
         />
 
-        <Button variant="contained" onClick={() => alert("Salvo!")}>
+
+        <Autocomplete
+          id="classe"
+          options={classes}
+          sx={{ marginBottom: 2 }}
+          getOptionLabel={(option: any) => option.nome}
+          onChange={(event, value: any) => setFormData({ ...formData, classe: (value.id) })}
+          renderInput={(params) => <TextField {...params} label="Classe" />}
+        />
+
+        <Box sx={{alignItems: "center", display: "flex", justifyContent: "space-between"}}>
+            <TextField
+              label="Intervenção"
+              value={intervencaoAtual.descricao}
+              onChange={handleDescricaoChange}
+              margin="normal"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={intervencaoAtual.opcional}
+                  onChange={handleOpcionalChange}
+                />
+              }
+              label="Opcional"
+            />
+            <Button variant="contained" sx={{backgroundColor: "#01828e", borderRadius: 25}} disabled={intervencaoAtual.descricao == ""} onClick={adicionarIntervencao}>
+              +
+            </Button>
+          </Box>
+          <Typography sx={{margin: "20px 0px", fontSize: "19px", color: "#01828e", fontWeight: 200}}>Intervenções</Typography>
+          <Box sx={{backgroundColor: "#e7e7e7", borderRadius: 2, padding: "20px", minHeight: "150px",  marginBottom: "20px"}}>
+            <ul>
+            {
+            intervencoes.length > 0 ?
+            intervencoes.map((item: any) => (
+              <Typography sx={{fontSize: "14px"}}>{item.descricao}  {item.opcional ? "(Opicional)" : "(Obrigatório)"}</Typography>
+            ))
+            :
+            <Box sx={{width: "100%", height: "150px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+              <Typography sx={{margin: "20px 0px", fontSize: "16px", fontWeight: 200}}>Nenhuma intervenção</Typography>
+            </Box>
+            }
+            </ul>
+          </Box>
+
+          
+        <TextField
+          name="resultadosEsperados"
+          placeholder="Resultados esperados"
+          sx={{ marginBottom: 2 }}
+          onChange={handleInputChange}
+
+        />
+
+        <Button variant="contained" sx={{backgroundColor: "#01828e"}} onClick={() => handleSave()}>
           Salvar
         </Button>
       </Box>
+      <ToastContainer/>
     </Box>
   );
 }
 
 export default App;
 
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-  { title: 'Casablanca', year: 1942 },
-  { title: 'City Lights', year: 1931 },
-  { title: 'Psycho', year: 1960 },
-  { title: 'The Green Mile', year: 1999 },
-  { title: 'The Intouchables', year: 2011 },
-  { title: 'Modern Times', year: 1936 },
-  { title: 'Raiders of the Lost Ark', year: 1981 },
-  { title: 'Rear Window', year: 1954 },
-  { title: 'The Pianist', year: 2002 },
-  { title: 'The Departed', year: 2006 },
-  { title: 'Terminator 2: Judgment Day', year: 1991 },
-  { title: 'Back to the Future', year: 1985 },
-  { title: 'Whiplash', year: 2014 },
-  { title: 'Gladiator', year: 2000 },
-  { title: 'Memento', year: 2000 },
-  { title: 'The Prestige', year: 2006 },
-  { title: 'The Lion King', year: 1994 },
-  { title: 'Apocalypse Now', year: 1979 },
-  { title: 'Alien', year: 1979 },
-  { title: 'Sunset Boulevard', year: 1950 },
-  {
-    title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964,
-  },
-  { title: 'The Great Dictator', year: 1940 },
-  { title: 'Cinema Paradiso', year: 1988 },
-  { title: 'The Lives of Others', year: 2006 },
-  { title: 'Grave of the Fireflies', year: 1988 },
-  { title: 'Paths of Glory', year: 1957 },
-  { title: 'Django Unchained', year: 2012 },
-  { title: 'The Shining', year: 1980 },
-  { title: 'WALL·E', year: 2008 },
-  { title: 'American Beauty', year: 1999 },
-  { title: 'The Dark Knight Rises', year: 2012 },
-  { title: 'Princess Mononoke', year: 1997 },
-  { title: 'Aliens', year: 1986 },
-  { title: 'Oldboy', year: 2003 },
-  { title: 'Once Upon a Time in America', year: 1984 },
-  { title: 'Witness for the Prosecution', year: 1957 },
-  { title: 'Das Boot', year: 1981 },
-  { title: 'Citizen Kane', year: 1941 },
-  { title: 'North by Northwest', year: 1959 },
-  { title: 'Vertigo', year: 1958 },
-  {
-    title: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983,
-  },
-  { title: 'Reservoir Dogs', year: 1992 },
-  { title: 'Braveheart', year: 1995 },
-  { title: 'M', year: 1931 },
-  { title: 'Requiem for a Dream', year: 2000 },
-  { title: 'Amélie', year: 2001 },
-  { title: 'A Clockwork Orange', year: 1971 },
-  { title: 'Like Stars on Earth', year: 2007 },
-  { title: 'Taxi Driver', year: 1976 },
-  { title: 'Lawrence of Arabia', year: 1962 },
-  { title: 'Double Indemnity', year: 1944 },
-  {
-    title: 'Eternal Sunshine of the Spotless Mind',
-    year: 2004,
-  },
-  { title: 'Amadeus', year: 1984 },
-  { title: 'To Kill a Mockingbird', year: 1962 },
-  { title: 'Toy Story 3', year: 2010 },
-  { title: 'Logan', year: 2017 },
-  { title: 'Full Metal Jacket', year: 1987 },
-  { title: 'Dangal', year: 2016 },
-  { title: 'The Sting', year: 1973 },
-  { title: '2001: A Space Odyssey', year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: 'Toy Story', year: 1995 },
-  { title: 'Bicycle Thieves', year: 1948 },
-  { title: 'The Kid', year: 1921 },
-  { title: 'Inglourious Basterds', year: 2009 },
-  { title: 'Snatch', year: 2000 },
-  { title: '3 Idiots', year: 2009 },
-  { title: 'Monty Python and the Holy Grail', year: 1975 },
-];
